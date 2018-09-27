b9185c7e73ae4904a85996b9fde95ec9a9d80025
Dileep Kishore <k.dileep1994@gmail.com>
2018-09-26 21:47:25 -0400

    Add utils.jl
    Contains utilities to read networks from files in various formats
    Right now it can only read edge lists from a file

---
+++ src/utils.jl
@@ -0,0 +1,20 @@
+function read_file(fname::String)::Tuple{Int64, Int64, Array{Array{Int64, 1}, 1}}
+    n_nodes, n_arcs, edge_list = open(fname) do file
+        counter = 1
+        n_nodes = 0
+        n_arcs = 0
+        edge_list = Array{Int64, 1}[]
+        for l in eachline(file)
+            if counter == 1
+                n_nodes, n_arcs = map(x -> parse(Int64, x), split(l))
+            else
+                a, b = split(l)
+                push!(edge_list, [parse(Int64, a), parse(Int64, b)])
+            end
+            counter += 1
+        end
+        (n_nodes, n_arcs, edge_list)
+    end
+    sorted_elist = sort(edge_list)
+    return n_nodes, n_arcs, sorted_elist
+end
