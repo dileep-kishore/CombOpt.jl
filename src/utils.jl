4b4919b029e31fee2fadbb17c19b9ab0ee7f9c0e
Dileep Kishore <k.dileep1994@gmail.com>
2018-10-06 21:02:52 -0400

    Update `read_elist` to also read 'weighted' graph input

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,4 @@
-function read_elist(fname::String)::Tuple{Int64, Int64, Array{Array{Int64, 1}, 1}}
+function read_elist(fname::String, weighted::Bool)::Tuple{Int64, Int64, Array{Array{Int64, 1}, 1}}
     n_nodes, n_arcs, edge_list = open(fname) do file
         counter = 1
         n_nodes = 0
@@ -6,10 +6,13 @@ function read_elist(fname::String)::Tuple{Int64, Int64, Array{Array{Int64, 1}, 1
         edge_list = Array{Int64, 1}[]
         for l in eachline(file)
             if counter == 1
-                n_nodes, n_arcs = map(x -> parse(Int64, x), split(l))
+                if weighted
+                    n_nodes, n_arcs, l_arc = map(x -> parse(Int64, x), split(l))
+                else
+                    n_nodes, n_arcs = map(x -> parse(Int64, x), split(l))
+                end
             else
-                a, b = split(l)
-                push!(edge_list, [parse(Int64, a), parse(Int64, b)])
+                push!(edge_list, map(x -> parse(Int64, x), split(l)))
             end
             counter += 1
         end
