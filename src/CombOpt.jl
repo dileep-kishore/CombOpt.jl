8e4db59c09bcbbcac80f3466c2240d7459d7f7f9
Dileep Kishore <k.dileep1994@gmail.com>
2018-10-24 21:24:49 -0400

    Create a proper module structure

--- src/CombOpt.jl
+++ src/CombOpt.jl
@@ -1,6 +1,13 @@
 module CombOpt
-export forward_star
 
-include("forward_star.jl")
+    using DelimitedFiles
+
+    export Node, Edge, Graph, adj
+    export read_graph
+    export dijkstra
+
+    include("graph.jl")
+    include("utils.jl")
+    include("dijkstra.jl")
 
 end
