b4a7357b9493e61966f2072781e1bfc3a1de770d
Dileep Kishore <k.dileep1994@gmail.com>
2018-10-24 20:53:53 -0400

    Refactor `forward_star` and add documentation

--- src/forward_star.jl
+++ src/forward_star.jl
@@ -1,23 +1,23 @@
-function forward_star(n_nodes::Int64, n_arcs::Int64, e_list::Array{Array{Int64, 1}, 1})::Tuple{Array{Int64, 1}, Array{Int64, 1}, Array{Int64, 1}}
+"""
+    forward_star(n_nodes::Int64, edge_list::Array{Int64, 2})
+
+Creates the forward star representation of the input edge list
+"""
+function forward_star(n_nodes::Int64, edge_list::Array{Int64, 2})::Tuple{Array{Int64, 1}, Array{Int64, 1}}
     first_out = ones(Int64, n_nodes + 1)
-    end_node = zeros(Int64, n_arcs)
-    weights = zeros(Int64, n_arcs)
+    n_edges = size(edge_list, 1)
+    end_node = zeros(Int64, n_edges)
     ind = 1
-    if len(e_list[1]) > 2
-        weighted = true
-    else
-        weighted = false
-    end
     for node in 1:n_nodes
-        if ind > n_arcs
+        if ind > n_edges
             first_out[node] = ind
+            continue
         end
-        for (i, edge) in enumerate(e_list[ind:end])
-            if edge[1] == node
-                end_node[ind] = edge[2]
-                if weighted
-                    weight[ind] = edge[3]
-                end
+        for i in size(edge_list, 1)
+            source = edge_list[i, 1]
+            target = edge_list[i, 2]
+            if source == node
+                end_node[ind] = target
                 ind += 1
             else
                 first_out[node + 1] = ind
@@ -26,5 +26,7 @@ function forward_star(n_nodes::Int64, n_arcs::Int64, e_list::Array{Array{Int64, 
         end
     end
     first_out[end] = ind
-    return first_out, end_node, weights
+    return first_out, end_node
 end
+
+# TODO: Implement reverse_star and integrate that with forward_star
