455c7b896cb7395bb3a46b5651c021f81251e4c2
Dileep Kishore <k.dileep1994@gmail.com>
2018-10-25 21:16:23 -0400

    Specify `dims` parameter for `sort` function

--- src/utils.jl
+++ src/utils.jl
@@ -8,7 +8,7 @@ function read_graph(fname::String, format::String, delim::AbstractChar=' ')::Gra
     n_nodes, n_edges, = map(x -> parse(Int64, x), header)
     @assert n_edges == size(data, 1) "Edge numbers don't match"
     nodes = map(x -> Node(x, 0, 0), 1:n_nodes)
-    sorted_data = sort(data)
+    sorted_data = sort(data, dims = 1)
     first_out, end_node = forward_star(n_nodes, sorted_data[:, 1:2])
     if format == "distance"
         edges = map(x -> Edge(x[0], x[1], 0), zip(1:n_edges, sorted_data[:, 3]))
