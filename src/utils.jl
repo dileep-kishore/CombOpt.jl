e883f6247481dddb0c892385cb25dd6c66605ca3
Dileep Kishore <k.dileep1994@gmail.com>
2018-10-25 21:10:10 -0400

    Use `assert` macro instead of function

--- src/utils.jl
+++ src/utils.jl
@@ -6,7 +6,7 @@ Reads in a delimited file with 3 columns into a `Graph` struct
 function read_graph(fname::String, format::String, delim::AbstractChar=' ')::Graph
     data, header = readdlm(fname, delim, Int64, header=true)
     n_nodes, n_edges, = map(x -> parse(Int64, x), header)
-    assert(n_edges == size(data, 1))
+    @assert n_edges == size(data, 1) "Edge numbers don't match"
     nodes = map(x -> Node(x, 0, 0), 1:n_nodes)
     sorted_data = sort(data)
     first_out, end_node = forward_star(n_nodes, sorted_data[:, 1:2])
@@ -19,7 +19,7 @@ function read_graph(fname::String, format::String, delim::AbstractChar=' ')::Gra
     else
         throw(ArgumentError("Unsupported format. Use either distance, capacity or assignment"))
     end
-    assert(len(first_out) == len(nodes) + 1)
-    assert(len(end_node) == len(edges))
+    @assert len(first_out) == len(nodes) + 1 "forward_star failed"
+    @assert len(end_node) == len(edges) "forward_star failed"
     return Graph(nodes, edges, first_out, end_node)
 end
