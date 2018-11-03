1528adfe1021ea1b9668390cfb3755965f2418d7
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-02 22:53:44 -0400

    Add: `bellman_ford` to `CombOpt.jl` module

--- src/CombOpt.jl
+++ src/CombOpt.jl
@@ -5,10 +5,12 @@ module CombOpt
     export Node, Edge, Graph, adj
     export read_graph
     export dijkstra
+    export bellman_ford
 
     include("graph.jl")
     include("forward_star.jl")
     include("utils.jl")
     include("dijkstra.jl")
+    include("bellman_ford.jl")
 
 end
