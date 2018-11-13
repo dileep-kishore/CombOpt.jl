1044ff6baff54daeca2b35914005ae43e1c0232e
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-12 21:40:15 -0500

    Tests: Set up entry points for the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,25 @@
 using CombOpt
+using Test
+
+@test isempty(detect_ambiguities(Base, Core, CombOpt))
+
+tests = [
+    "read_graph",
+    "dijkstra",
+    "bellman_ford",
+    "forward_star",
+]
+
+if length(ARGS) > 0
+    tests = ARGS
+end
+
+@testset "CombOpt" begin
+
+    for t in tests
+        fp = joinpath(dirname(@__FILE__), "test_$t.jl")
+        println("$fp ...")
+        include(fp)
+    end
+
+end
