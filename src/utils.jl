a2053d346dc137087eb9cd6cedf680cf3f3e87c1
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-12 21:42:48 -0500

    Fix: Remove assertions from `read_graph`
    These should be checked in the tests

--- src/utils.jl
+++ src/utils.jl
@@ -6,7 +6,6 @@ Reads in a delimited file with 2 (simple) or 3 columns into a `Graph` struct
 function read_graph(fname::String, format::String; delim::AbstractChar='\t')::Graph
     data, header = readdlm(fname, delim, Int64, header=true)
     n_nodes, n_edges, = map(x -> parse(Int64, x), header)
-    @assert n_edges == size(data, 1) "Edge numbers don't match"
     nodes = map(x -> Node(x, 0, 0), 1:n_nodes)
     sorted_data = sortslices(data, dims = 1)
     first_out, end_node = forward_star(n_nodes, sorted_data[:, 1:2])
@@ -21,7 +20,5 @@ function read_graph(fname::String, format::String; delim::AbstractChar='\t')::Gr
     else
         throw(ArgumentError("Unsupported format. Use either distance, capacity or assignment"))
     end
-    @assert length(first_out) == length(nodes) + 1 "forward_star failed"
-    @assert length(end_node) == length(edges) "forward_star failed"
     return Graph(nodes, edges, first_out, end_node)
 end
