aac97cb68983f46778272a392277ea2ca68408ae
Dileep Kishore <k.dileep1994@gmail.com>
2018-11-14 21:16:05 -0500

    Tests: Update `read_graph` testset with real tests
    Tests all four types of networks: "simple", "distance", "capacity" and
    "assignment"

--- test/test_read_graph.jl
+++ test/test_read_graph.jl
@@ -2,13 +2,52 @@
 
     using DelimitedFiles
 
-    @testset "simple_elist" begin
-        fname = "data/simple_elist.txt"
-        graph = read_graph(fname, "simple")
-        data, header = readdlm(fname, '\t', Int64, header=true)
-        @test length(graph.edges) == size(data, 1)
-        @test length(graph.first_out) == length(graph.nodes) + 1
-        @test length(graph.end_node) == length(graph.edges)
+    @testset "simple" begin
+        folder = "data/networks/simple"
+        for fname in readdir(folder)
+            file = joinpath(folder, fname)
+            graph = read_graph(file, "simple")
+            data, header = readdlm(file, '\t', Int64, header=true)
+            @test length(graph.edges) == size(data, 1)
+            @test length(graph.first_out) == length(graph.nodes) + 1
+            @test length(graph.end_node) == length(graph.edges)
+        end
+    end
+
+    @testset "distance" begin
+        folder = "data/networks/distance"
+        for fname in readdir(folder)
+            file = joinpath(folder, fname)
+            graph = read_graph(file, "distance")
+            data, header = readdlm(file, '\t', Int64, header=true)
+            @test length(graph.edges) == size(data, 1)
+            @test length(graph.first_out) == length(graph.nodes) + 1
+            @test length(graph.end_node) == length(graph.edges)
+        end
+    end
+
+    @testset "capacity" begin
+        folder = "data/networks/capacity"
+        for fname in readdir(folder)
+            file = joinpath(folder, fname)
+            graph = read_graph(file, "capacity")
+            data, header = readdlm(file, '\t', Int64, header=true)
+            @test length(graph.edges) == size(data, 1)
+            @test length(graph.first_out) == length(graph.nodes) + 1
+            @test length(graph.end_node) == length(graph.edges)
+        end
+    end
+
+    @testset "assignment" begin
+        folder = "data/networks/assignment"
+        for fname in readdir(folder)
+            file = joinpath(folder, fname)
+            graph = read_graph(file, "assignment")
+            data, header = readdlm(file, '\t', Int64, header=true)
+            @test length(graph.edges) == size(data, 1)
+            @test length(graph.first_out) == length(graph.nodes) + 1
+            @test length(graph.end_node) == length(graph.edges)
+        end
     end
 
 end
